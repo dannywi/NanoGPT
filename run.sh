f398012faa88b0edf59b9cb9570b95f6610ac143
Danny W <54228968+dannywi@users.noreply.github.com>
2023-05-02 22:12:50 +0900

    Adjust log output

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 # uncomment to download a file of your choice
 # curl -o input.txt https://www.ccel.org/d/dostoevsky/crime/crime.txt
 
-current_date_time=$(date)
-echo "====== RUNNING MODEL: $current_date_time ======" | tee -a log.txt
-time python bigram.py | tee -a log.txt
+echo "====== RUNNING MODEL:" $(date) "======" | tee -a log.txt
+time python bigram.py 2>&1 | tee -a log.txt
+echo "====== FINISHED:" $(date) "======" | tee -a log.txt
